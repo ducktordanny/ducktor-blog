8b205a9aa3c341007774827529e245c661c8c62c
Daniel Lazar <lazardani0112@gmail.com>
2021-12-19 12:23:09 +0100

    feat(login-sign-up-connection): Add navigation to auth pages

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  {
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'sign-up', component: SignUpComponent },
+    ],
+  },
 ];
 
 @NgModule({
