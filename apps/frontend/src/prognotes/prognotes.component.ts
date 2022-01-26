1a4876a1e4a888f99ce4f2edad46563c5ac59ec4
Daniel Lazar <lazardani0112@gmail.com>
2022-01-26 22:34:50 +0100

    feat(profile-page): Set up basic view of profile

--- apps/frontend/src/prognotes/prognotes.component.ts
+++ apps/frontend/src/prognotes/prognotes.component.ts
@@ -1,15 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {Router} from '@angular/router';
+
+import {BehaviorSubject, Subscription} from 'rxjs';
+
+import {AuthService} from './pages/auth/auth.service';
+import {UserService} from './pages/auth/user.service';
 
 @Component({
   selector: 'frontend-root',
   templateUrl: './prognotes.template.html',
   styleUrls: ['./prognotes.styles.scss'],
 })
-export class ProgNotesComponent {
-  title = 'client';
+export class ProgNotesComponent implements OnDestroy {
+  public isLoggedIn = new BehaviorSubject<boolean | undefined>(undefined);
+  private userSubscription: Subscription;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UserService,
+  ) {
+    this.userSubscription = this.userService.userInfo.subscribe((user) => {
+      this.isLoggedIn.next(user !== undefined);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
   public goToHome(): void {
     this.router.navigate(['']);
@@ -17,5 +35,13 @@ export class ProgNotesComponent {
 
   public navigateToAuth(pageName: string): void {
     this.router.navigate(['auth', pageName]);
+  }
+
+  public navigateToProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  public logout(): void {
+    this.authService.logoutUser();
   }
 }
